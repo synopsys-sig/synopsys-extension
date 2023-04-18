db3e0017ed91364c512147a1e544897989c6ef34
Kiran Narayana <kirann@synopsys.com>
2023-04-18 18:17:06 +0530

    pushing code changes.

--- synopsys-task/lib/synopsys-task/input-data/polaris.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PolarisAssessmentType = void 0;
-var PolarisAssessmentType;
-(function (PolarisAssessmentType) {
-    PolarisAssessmentType["SCA"] = "SCA";
-    PolarisAssessmentType["SAST"] = "SAST";
-})(PolarisAssessmentType = exports.PolarisAssessmentType || (exports.PolarisAssessmentType = {}));
