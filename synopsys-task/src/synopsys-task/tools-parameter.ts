9e5c7bb896b038da0e20896dfcd02f25b44adf86
Kishori Kumar <kishori.kumar@synopsys.com>
2023-04-20 00:01:41 +0530

    Polaris Scan

---
+++ synopsys-task/src/synopsys-task/tools-parameter.ts
@@ -0,0 +1,77 @@
+import * as fs from "fs";
+import path from "path";
+import * as inputs from "./inputs";
+import { Polaris } from "./input-data/polaris";
+import { InputData } from "./input-data/input-data";
+import * as constants from "./application-constants";
+import {debug} from "azure-pipelines-task-lib";
+
+export class SynopsysToolsParameter {
+  tempDir: string;
+  private static STAGE_OPTION = "--stage";
+  private static STATE_OPTION = "--state";
+  private static POLARIS_STAGE = "polaris";
+  private static POLARIS_STATE_FILE_NAME = "polaris_input.json";
+  private static SPACE = " ";
+
+  constructor(tempDir: string) {
+    this.tempDir = tempDir;
+  }
+
+  getFormattedCommandForPolaris(): string {
+    let command = "";
+    const assessmentTypeArray: string[] = [];
+    const assessmentTypes = inputs.POLARIS_ASSESSMENT_TYPES;
+    if (assessmentTypes != null && assessmentTypes.length > 0) {
+      try {
+        // converting provided assessmentTypes to uppercase
+        for (const assessmentType of assessmentTypes) {
+          const regEx = new RegExp("^[a-zA-Z]+$");
+          if (
+            assessmentType.trim().length > 0 &&
+            regEx.test(assessmentType.trim())
+          ) {
+            assessmentTypeArray.push(assessmentType.trim());
+          }
+        }
+      } catch (error) {
+        throw new Error(
+          "Invalid value for ".concat(constants.POLARIS_ASSESSMENT_TYPES_KEY)
+        );
+      }
+    }
+
+    const polData: InputData<Polaris> = {
+      data: {
+        polaris: {
+          accesstoken: inputs.POLARIS_ACCESS_TOKEN,
+          serverUrl: inputs.POLARIS_SERVER_URL,
+          application: { name: inputs.POLARIS_APPLICATION_NAME },
+          project: { name: inputs.POLARIS_PROJECT_NAME },
+          assessment: { types: assessmentTypeArray },
+        },
+      },
+    };
+
+    const inputJson = JSON.stringify(polData);
+
+    const stateFilePath = path.join(
+      this.tempDir,
+      SynopsysToolsParameter.POLARIS_STATE_FILE_NAME
+    );
+    fs.writeFileSync(stateFilePath, inputJson);
+
+    debug("Generated state json file content is - ".concat(inputJson));
+
+    command = SynopsysToolsParameter.STAGE_OPTION.concat(
+      SynopsysToolsParameter.SPACE
+    )
+      .concat(SynopsysToolsParameter.POLARIS_STAGE)
+      .concat(SynopsysToolsParameter.SPACE)
+      .concat(SynopsysToolsParameter.STATE_OPTION)
+      .concat(SynopsysToolsParameter.SPACE)
+      .concat(stateFilePath)
+      .concat(SynopsysToolsParameter.SPACE);
+    return command;
+  }
+}
--- synopsys-task/tests/_suite.ts
+++ synopsys-task/tests/_suite.ts
@@ -1,48 +1,55 @@
-import * as path from 'path';
-import * as assert from 'assert';
-import * as ttm from 'azure-pipelines-task-lib/mock-test';
+import * as path from "path";
+import * as assert from "assert";
+import * as ttm from "azure-pipelines-task-lib/mock-test";
 
-describe('Sample task tests', function () {
+describe("Sample task tests", function () {
+  before(function () {});
 
-    before( function() {
+  after(() => {});
 
-    });
+  it("should succeed with simple inputs", function (done: Mocha.Done) {
+    this.timeout(1000);
 
-    after(() => {
+    let tp = path.join(__dirname, "success.js");
+    let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
 
-    });
+    tr.run();
+    console.log(tr.succeeded);
+    assert.equal(tr.succeeded, true, "should have succeeded");
+    assert.equal(tr.warningIssues.length, 0, "should have no warnings");
+    assert.equal(tr.errorIssues.length, 0, "should have no errors");
+    console.log(tr.stdout);
+    assert.equal(
+      tr.stdout.indexOf("Hello human") >= 0,
+      true,
+      "should display Hello human"
+    );
 
-    it('should succeed with simple inputs', function(done: Mocha.Done) {
-        this.timeout(1000);
+    done();
+  });
 
-        let tp = path.join(__dirname, 'success.js');
-        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+  it("it should fail if tool returns 1", function (done: Mocha.Done) {
+    this.timeout(1000);
 
-        tr.run();
-        console.log(tr.succeeded);
-        assert.equal(tr.succeeded, true, 'should have succeeded');
-        assert.equal(tr.warningIssues.length, 0, "should have no warnings");
-        assert.equal(tr.errorIssues.length, 0, "should have no errors");
-        console.log(tr.stdout);
-        assert.equal(tr.stdout.indexOf('Hello human') >= 0, true, "should display Hello human");
+    let tp = path.join(__dirname, "failure.js");
+    let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
 
-        done();
-    });
+    tr.run();
+    console.log(tr.succeeded);
+    assert.equal(tr.succeeded, false, "should have failed");
+    assert.equal(tr.warningIssues.length, 0, "should have no warnings");
+    assert.equal(tr.errorIssues.length, 1, "should have 1 error issue");
+    assert.equal(
+      tr.errorIssues[0],
+      "Bad input was given",
+      "error issue output"
+    );
+    assert.equal(
+      tr.stdout.indexOf("Hello bad"),
+      -1,
+      "Should not display Hello bad"
+    );
 
-    it('it should fail if tool returns 1', function(done: Mocha.Done) {
-        this.timeout(1000);
-
-        let tp = path.join(__dirname, 'failure.js');
-        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-
-        tr.run();
-        console.log(tr.succeeded);
-        assert.equal(tr.succeeded, false, 'should have failed');
-        assert.equal(tr.warningIssues.length, 0, "should have no warnings");
-        assert.equal(tr.errorIssues.length, 1, "should have 1 error issue");
-        assert.equal(tr.errorIssues[0], 'Bad input was given', 'error issue output');
-        assert.equal(tr.stdout.indexOf('Hello bad'), -1, "Should not display Hello bad");
-
-        done();
-    });
+    done();
+  });
 });
