718435b6bd10dbea6ba3b3377a156e8e93a203fe
Kishori Kumar <kishori.kumar@synopsys.com>
2023-04-27 12:00:01 +0530

    Polaris scan with bridge download, unzip and execution refactoring

--- synopsys-task/src/synopsys-task/tools-parameter.ts
+++ synopsys-task/src/synopsys-task/tools-parameter.ts
@@ -1,8 +1,8 @@
 import * as fs from "fs";
 import path from "path";
 import * as inputs from "./inputs";
-import { Polaris } from "./input-data/polaris";
-import { InputData } from "./input-data/input-data";
+import { Polaris } from "./bean/polaris";
+import { InputData } from "./bean/input-data";
 import * as constants from "./application-constants";
 import { debug } from "azure-pipelines-task-lib";
 
--- synopsys-task/src/synopsys-task/utility.ts
+++ synopsys-task/src/synopsys-task/utility.ts
@@ -3,6 +3,7 @@ import path from "path";
 import { SYNOPSYS_BRIDGE_ZIP_FILE_NAME } from "./application-constants";
 import * as toolLib from "azure-pipelines-tool-lib";
 import * as process from "process";
+import { DownloadFileResponse } from "./bean/download-file-response";
 
 export function cleanUrl(url: string): string {
   if (url && url.endsWith("/")) {
@@ -13,11 +14,6 @@ export function cleanUrl(url: string): string {
 
 export function getTempDir(): string {
   return process.env["AGENT_TEMPDIRECTORY"] || "";
-}
-
-export interface DownloadFileResponse {
-  filePath: string;
-  fileName: string;
 }
 
 export async function extractZipped(
