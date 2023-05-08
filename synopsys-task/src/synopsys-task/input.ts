adecd5a260da1b46c1816dc0519ed9bff37669d1
Kiran Narayana <kirann@synopsys.com>
2023-05-08 12:31:23 +0530

    Adding Blackduck configuration

--- synopsys-task/src/synopsys-task/input.ts
+++ synopsys-task/src/synopsys-task/input.ts
@@ -42,7 +42,7 @@ export const BLACKDUCK_URL =
 export const BLACKDUCK_API_TOKEN =
   taskLib.getInput(constants.BLACKDUCK_API_TOKEN_KEY) || "";
 export const BLACKDUCK_INSTALL_DIRECTORY =
-  taskLib.getInput(constants.BLACKDUCK_INSTALL_DIRECTORY_KEY) || "";
+  taskLib.getPathInput(constants.BLACKDUCK_INSTALL_DIRECTORY_KEY) || "";
 export const BLACKDUCK_SCAN_FULL =
   taskLib.getInput(constants.BLACKDUCK_SCAN_FULL_KEY) || "";
 export const BLACKDUCK_SCAN_FAILURE_SEVERITIES =
