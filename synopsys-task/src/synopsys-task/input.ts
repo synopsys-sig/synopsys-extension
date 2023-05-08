70872e1107717b857057d0448a53187b318a603e
Kiran Narayana <kirann@synopsys.com>
2023-05-08 11:30:48 +0530

    Adding Blackduck configuration

--- synopsys-task/src/synopsys-task/input.ts
+++ synopsys-task/src/synopsys-task/input.ts
@@ -22,8 +22,21 @@ export const POLARIS_ASSESSMENT_TYPES = taskLib.getDelimitedInput(
 export const POLARIS_SERVER_URL =
   taskLib.getInput(constants.POLARIS_SERVER_URL_KEY) || "";
 
-export const COVERITY_AUTOMATION_PRCOMMENT =
-  taskLib.getInput(constants.COVERITY_AUTOMATION_PRCOMMENT_KEY) || "";
+// Coverity related inputs
+export const COVERITY_URL = taskLib.getInput(constants.COVERITY_URL_KEY) || "";
+export const COVERITY_USER =
+  taskLib.getInput(constants.COVERITY_USER_NAME_KEY) || "";
+export const COVERITY_USER_PASSWORD =
+  taskLib.getInput(constants.COVERITY_USER_PASSWORD_KEY) || "";
+export const COVERITY_PROJECT_NAME =
+  taskLib.getInput(constants.COVERITY_PROJECT_NAME_KEY) || "";
+export const COVERITY_STREAM_NAME =
+  taskLib.getInput(constants.COVERITY_STREAM_NAME_KEY) || "";
+export const COVERITY_INSTALL_DIRECTORY =
+  taskLib.getPathInput(constants.COVERITY_INSTALL_DIRECTORY_KEY) || "";
+export const COVERITY_POLICY_VIEW =
+  taskLib.getInput(constants.COVERITY_POLICY_VIEW_KEY) || "";
+
 
 export const BLACKDUCK_URL =
   taskLib.getInput(constants.BLACKDUCK_URL_KEY) || "";
@@ -41,17 +54,3 @@ export const BLACKDUCK_AUTOMATION_PRCOMMENT =
   taskLib.getInput(constants.BLACKDUCK_AUTOMATION_PRCOMMENT_KEY) || "";
 
 export const GITHUB_TOKEN = taskLib.getInput(constants.GITHUB_TOKEN_KEY) || "";
-
-export const COVERITY_USER =
-  taskLib.getInput(constants.COVERITY_USER_NAME_KEY) || "";
-export const COVERITY_URL = taskLib.getInput(constants.COVERITY_URL_KEY) || "";
-export const COVERITY_PROJECT_NAME =
-  taskLib.getInput(constants.COVERITY_PROJECT_NAME_KEY) || "";
-export const COVERITY_STREAM_NAME =
-  taskLib.getInput(constants.COVERITY_STREAM_NAME_KEY) || "";
-export const COVERITY_INSTALL_DIRECTORY =
-  taskLib.getPathInput(constants.COVERITY_INSTALL_DIRECTORY_KEY) || "";
-export const COVERITY_POLICY_VIEW =
-  taskLib.getInput(constants.COVERITY_POLICY_VIEW_KEY) || "";
-export const COVERITY_USER_PASSWORD =
-  taskLib.getInput(constants.COVERITY_USER_PASSWORD_KEY) || "";
