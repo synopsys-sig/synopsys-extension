4ae2976a3a71a68f777ab63f54500dfa2738c655
spurohit <spurohit@synopsys.com>
2023-05-12 18:06:57 +0530

    bridge diagnostics upload test cases

--- synopsys-task/src/synopsys-task/diagnostics.ts
+++ synopsys-task/src/synopsys-task/diagnostics.ts
@@ -2,21 +2,20 @@ import * as taskLib from "azure-pipelines-task-lib/task";
 import * as constants from "./application-constant";
 
 export async function uploadDiagnostics(workspaceDir: string) {
-  try {
-    const uploadArtifactPath = workspaceDir.concat(getBridgeDiagnosticsFolder());
-    let isBridgeDirectoryExists = false;
-    isBridgeDirectoryExists = taskLib.exist(uploadArtifactPath);
-    if (isBridgeDirectoryExists) {
-      await taskLib.uploadArtifact(
-        constants.UPLOAD_ARTIFACT_NAEME,
-        uploadArtifactPath,
-        constants.UPLOAD_ARTIFACT_NAEME
-      );
-    }
-  } catch (error) {
-    throw(error)
+  // try {
+  const uploadArtifactPath = workspaceDir.concat(getBridgeDiagnosticsFolder());
+  let isBridgeDirectoryExists = false;
+  isBridgeDirectoryExists = taskLib.exist(uploadArtifactPath);
+  if (isBridgeDirectoryExists) {
+    await taskLib.uploadArtifact(
+      constants.UPLOAD_ARTIFACT_NAEME,
+      uploadArtifactPath,
+      constants.UPLOAD_ARTIFACT_NAEME
+    );
   }
-  
+  // } catch (error) {
+  //   throw error;
+  // }
 }
 
 function getBridgeDiagnosticsFolder(): string {
