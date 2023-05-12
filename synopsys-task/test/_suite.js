1cb7c712aa09bd4efa674e37659d5c5026020e1c
Kiran Narayana <kirann@synopsys.com>
2023-05-12 12:02:14 +0530

    pushing initial commits

--- synopsys-task/test/_suite.js
+++ synopsys-task/test/_suite.js
@@ -1,30 +1,55 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 var path = require("path");
 var assert = require("assert");
 var ttm = require("azure-pipelines-task-lib/mock-test");
 describe("Sample task tests", function () {
     before(function () { });
     after(function () { });
-    it("should succeed with simple inputs", function (done) {
+    // it("should succeed with simple inputs", function (done: Mocha.Done) {
+    //   this.timeout(1000);
+    //   let tp = path.join(__dirname, "success.js");
+    //   let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+    //   tr.run();
+    //   console.log("tr.succeeded" + tr.succeeded);
+    //   assert.equal(tr.succeeded, true, "should have succeeded");
+    //   assert.equal(tr.warningIssues.length, 0, "should have no warnings");
+    //   assert.equal(tr.errorIssues.length, 0, "should have no errors");
+    //   console.log("*************************************************tr.stdout::::::" + tr.stdout);
+    //   assert.equal(
+    //     tr.stdout.indexOf("Hello human") >= 0,
+    //     true,
+    //     "should display Hello human"
+    //   );
+    //   done();
+    // });
+    // it("it should fail if tool returns 1", function (done: Mocha.Done) {
+    //   this.timeout(1000);
+    //   let tp = path.join(__dirname, "failure.js");
+    //   let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+    //   tr.run();
+    //   console.log(tr.succeeded);
+    //   assert.equal(tr.succeeded, false, "should have failed");
+    //   assert.equal(tr.warningIssues.length, 0, "should have no warnings");
+    //   assert.equal(tr.errorIssues.length, 1, "should have 1 error issue");
+    //   assert.equal(
+    //     tr.errorIssues[0],
+    //     "Bad input was given",
+    //     "error issue output"
+    //   );
+    //   assert.equal(
+    //     tr.stdout.indexOf("Hello bad"),
+    //     -1,
+    //     "Should not display Hello bad"
+    //   );
+    //   done();
+    // });
+    it("Polaris", function (done) {
         this.timeout(1000);
-        var tp = path.join(__dirname, "success.js");
+        var tp = path.join(__dirname, "polaris.js");
         var tr = new ttm.MockTestRunner(tp);
         tr.run();
-        console.log(tr.succeeded);
-        assert.equal(tr.succeeded, true, "should have succeeded");
-        assert.equal(tr.warningIssues.length, 0, "should have no warnings");
-        assert.equal(tr.errorIssues.length, 0, "should have no errors");
-        console.log(tr.stdout);
-        assert.equal(tr.stdout.indexOf("Hello human") >= 0, true, "should display Hello human");
-        done();
-    });
-    it("it should fail if tool returns 1", function (done) {
-        this.timeout(1000);
-        var tp = path.join(__dirname, "failure.js");
-        var tr = new ttm.MockTestRunner(tp);
-        tr.run();
-        console.log(tr.succeeded);
+        console.log("tr.stdout" + JSON.stringify(tr.stdout));
         assert.equal(tr.succeeded, false, "should have failed");
         assert.equal(tr.warningIssues.length, 0, "should have no warnings");
         assert.equal(tr.errorIssues.length, 1, "should have 1 error issue");
--- synopsys-task/test/success.ts
+++ synopsys-task/test/success.ts
@@ -2,10 +2,10 @@ import * as path from 'path';
 import * as tmrm from "azure-pipelines-task-lib/mock-run";
 import * as ta from "azure-pipelines-task-lib/mock-answer";
 
-let taskPath = path.join(__dirname, "..", "dist" , "index.js");
-console.log("taskPATH::::::" + taskPath)
+
+let taskPath = "/Users/kirann/IdeaProjects/synopsys-extension-bduck/synopsys-task/dist/kiranindex.js"
 let tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
 
-tmr.setInput("samplestring", "bad");
+tmr.setInput('samplestring', 'human');
 
 tmr.run();
