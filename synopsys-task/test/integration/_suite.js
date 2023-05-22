6abca5e1b8ea8b2212f2e43a9f08a4814e32607d
sig-rnd-io-testuser <80479185+sig-rnd-io-testuser@users.noreply.github.com>
2023-05-22 16:56:56 +0530

    Integration test (#17)
    Co-authored-by: Kishori Kumar <kishori.kumar@synopsys.com>
    Co-authored-by: Kiran Narayana <kirann@synopsys.com>

--- synopsys-task/test/integration/_suite.js
+++ synopsys-task/test/integration/_suite.js
@@ -40,9 +40,51 @@ describe("Sample task tests", function () {
         assert.notEqual(tr.stdout.indexOf("Synopsys Action workflow execution completed"), -1, "should have the specified log");
         done();
     });
+    it("should succeed with coverity input", function (done) {
+        this.timeout(2000);
+        let tp = path.join(__dirname, "coverity-scan-success.js");
+        let tr = new ttm.MockTestRunner(tp);
+        tr.run();
+        assert.equal(tr.succeeded, true, "should have succeeded");
+        assert.equal(tr.warningIssues.length, 0, "should have no warnings");
+        assert.equal(tr.errorIssues.length, 0, "should have 0 error issue");
+        assert.notEqual(tr.stdout.indexOf("Synopsys Action workflow execution completed"), -1, "should have the specified log");
+        //assert.equal(tr.stdout.indexOf("blackduckErrors"), -1, "blackduckErrors");
+        done();
+    });
+    it("should succeed with blackduck input", function (done) {
+        this.timeout(2000);
+        let tp = path.join(__dirname, "blackduck-scan-success.js");
+        let tr = new ttm.MockTestRunner(tp);
+        tr.run();
+        assert.equal(tr.succeeded, true, "should have succeeded");
+        assert.equal(tr.warningIssues.length, 0, "should have no warnings");
+        assert.equal(tr.errorIssues.length, 0, "should have 0 error issue");
+        assert.notEqual(tr.stdout.indexOf("Synopsys Action workflow execution completed"), -1, "should have the specified log");
+        //assert.equal(tr.stdout.indexOf("blackduckErrors"), -1, "blackduckErrors");
+        done();
+    });
     it('should fail with polaris input', function () {
         this.timeout(2000);
         let tp = path.join(__dirname, "polaris-scan-failure.js");
+        let tr = new ttm.MockTestRunner(tp);
+        tr.run();
+        assert.equal(tr.failed, true, "should have failed");
+        assert.equal(tr.errorIssues.length, 2, "should have 1 error issue");
+        assert.equal(tr.errorIssues[1], "Workflow failed! Exit Code: 2 Error from adapter end");
+    });
+    it('should fail with coverity input', function () {
+        this.timeout(2000);
+        let tp = path.join(__dirname, "coverity-scan-failure.js");
+        let tr = new ttm.MockTestRunner(tp);
+        tr.run();
+        assert.equal(tr.failed, true, "should have failed");
+        assert.equal(tr.errorIssues.length, 2, "should have 1 error issue");
+        assert.equal(tr.errorIssues[1], "Workflow failed! Exit Code: 2 Error from adapter end");
+    });
+    it('should fail with blackduck input', function () {
+        this.timeout(2000);
+        let tp = path.join(__dirname, "blackduck-scan-failure.js");
         let tr = new ttm.MockTestRunner(tp);
         tr.run();
         assert.equal(tr.failed, true, "should have failed");
