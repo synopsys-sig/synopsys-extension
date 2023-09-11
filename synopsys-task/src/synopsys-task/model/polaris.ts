4525b1f5906b6dcd8f44e2a813cd939d40153acc
lokesh <lokesha@synopsys.com>
2023-09-11 14:26:52 +0530

    feat: SIGINT-1101 polaris branching parameters

--- synopsys-task/src/synopsys-task/model/polaris.ts
+++ synopsys-task/src/synopsys-task/model/polaris.ts
@@ -11,6 +11,11 @@ export interface PolarisData {
   accesstoken: string;
   serverUrl: string;
   application: { name: string };
+  branch: Branch;
   project: { name: string };
   assessment: { types: string[] };
+}
+
+export interface Branch {
+  name?: string;
 }
