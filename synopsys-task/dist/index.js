7857f894bd3f259885f97b7bbfe990d4565fd541
Akib Uz Zaman <akibuz@synopsys.com>
2024-04-01 14:12:53 +0600

    update dist files

--- synopsys-task/dist/index.js
+++ synopsys-task/dist/index.js
@@ -573,7 +573,7 @@ const taskLib = __importStar(__nccwpck_require__(347));
 const constants = __importStar(__nccwpck_require__(3051));
 //Bridge download url
 exports.BRIDGE_DOWNLOAD_URL = ((_a = taskLib.getInput("bridge_download_url")) === null || _a === void 0 ? void 0 : _a.trim()) || "";
-exports.ENABLE_NETWORK_AIRGAP = taskLib.getBoolInput("bridge_network_airgap") || false;
+exports.ENABLE_NETWORK_AIRGAP = taskLib.getBoolInput("bridge_network_airgap") || "";
 exports.SYNOPSYS_BRIDGE_INSTALL_DIRECTORY_KEY = taskLib.getPathInput("synopsys_bridge_install_directory", false, false) || "";
 exports.BRIDGE_DOWNLOAD_VERSION = ((_b = taskLib.getPathInput("bridge_download_version")) === null || _b === void 0 ? void 0 : _b.trim()) || "";
 // Polaris related inputs
@@ -623,7 +623,7 @@ exports.COVERITY_PROJECT_NAME = ((_z = taskLib.getInput(constants.COVERITY_PROJE
 exports.COVERITY_STREAM_NAME = ((_0 = taskLib.getInput(constants.COVERITY_STREAM_NAME_KEY)) === null || _0 === void 0 ? void 0 : _0.trim()) || "";
 exports.COVERITY_INSTALL_DIRECTORY = ((_1 = taskLib.getPathInput(constants.COVERITY_INSTALL_DIRECTORY_KEY)) === null || _1 === void 0 ? void 0 : _1.trim()) || "";
 exports.COVERITY_POLICY_VIEW = ((_2 = taskLib.getInput(constants.COVERITY_POLICY_VIEW_KEY)) === null || _2 === void 0 ? void 0 : _2.trim()) || "";
-exports.COVERITY_LOCAL = ((_3 = taskLib.getInput(constants.COVERITY_LOCAL_KEY)) === null || _3 === void 0 ? void 0 : _3.trim()) === "true" || false;
+exports.COVERITY_LOCAL = ((_3 = taskLib.getInput(constants.COVERITY_LOCAL_KEY)) === null || _3 === void 0 ? void 0 : _3.trim()) || "";
 exports.COVERITY_AUTOMATION_PRCOMMENT = taskLib.getInput(constants.COVERITY_AUTOMATION_PRCOMMENT_KEY) || "";
 exports.COVERITY_VERSION = ((_4 = taskLib.getInput(constants.COVERITY_VERSION_KEY)) === null || _4 === void 0 ? void 0 : _4.trim()) || "";
 // Blackduck related inputs
@@ -1290,7 +1290,6 @@ class SynopsysToolsParameter {
                     application: {},
                     project: {},
                     assessment: { types: assessmentTypeArray },
-                    branch: {},
                 },
             },
         };
@@ -1308,7 +1307,7 @@ class SynopsysToolsParameter {
             polData.data.polaris.project.name = azureRepositoryName;
         }
         if (inputs.POLARIS_BRANCH_NAME) {
-            polData.data.polaris.branch.name = inputs.POLARIS_BRANCH_NAME;
+            polData.data.polaris.branch = { name: inputs.POLARIS_BRANCH_NAME };
         }
         if (inputs.POLARIS_TRIAGE) {
             polData.data.polaris.triage = inputs.POLARIS_TRIAGE;
@@ -1368,10 +1367,6 @@ class SynopsysToolsParameter {
                     blackduck: {
                         url: inputs.BLACKDUCK_URL,
                         token: inputs.BLACKDUCK_API_TOKEN,
-                        automation: {},
-                    },
-                    network: {
-                        airGap: inputs.ENABLE_NETWORK_AIRGAP,
                     },
                 },
             };
@@ -1381,15 +1376,14 @@ class SynopsysToolsParameter {
                 };
             }
             if (inputs.BLACKDUCK_SCAN_FULL) {
-                let scanFullValue = false;
                 if (inputs.BLACKDUCK_SCAN_FULL.toLowerCase() === "true" ||
                     inputs.BLACKDUCK_SCAN_FULL.toLowerCase() === "false") {
-                    scanFullValue = inputs.BLACKDUCK_SCAN_FULL.toLowerCase() === "true";
+                    const scanFullValue = inputs.BLACKDUCK_SCAN_FULL.toLowerCase() === "true";
+                    blackduckData.data.blackduck.scan = { full: scanFullValue };
                 }
                 else {
                     throw new Error("Missing boolean value for ".concat(constants.BLACKDUCK_SCAN_FULL_KEY));
                 }
-                blackduckData.data.blackduck.scan = { full: scanFullValue };
             }
             if (failureSeverities && failureSeverities.length > 0) {
                 (0, validator_1.validateBlackduckFailureSeverities)(failureSeverities);
@@ -1423,16 +1417,15 @@ class SynopsysToolsParameter {
                 blackduckData.data.blackduck.fixpr = this.setBlackDuckFixPrInputs();
                 blackduckData.data.azure = yield this.getAzureRepoInfo();
             }
-            else {
-                // Disable fix pull request for adapters
-                blackduckData.data.blackduck.fixpr = { enabled: false };
-            }
             if ((0, utility_1.parseToBoolean)(inputs.BLACKDUCK_AUTOMATION_PRCOMMENT)) {
                 console.info("BlackDuck Automation comment is enabled");
                 blackduckData.data.azure = yield this.getAzureRepoInfo();
                 blackduckData.data.environment = this.setEnvironmentScanPullData();
-                blackduckData.data.blackduck.automation.prcomment = true;
+                blackduckData.data.blackduck.automation = { prcomment: true };
                 blackduckData.data;
+            }
+            if ((0, utility_1.parseToBoolean)(inputs.ENABLE_NETWORK_AIRGAP)) {
+                blackduckData.data.network = { airGap: true };
             }
             const buildReason = taskLib.getVariable(azure_1.AZURE_ENVIRONMENT_VARIABLES.AZURE_BUILD_REASON) || "";
             if ((0, utility_1.parseToBoolean)(inputs.BLACKDUCK_REPORTS_SARIF_CREATE) ||
@@ -1477,12 +1470,7 @@ class SynopsysToolsParameter {
                             project: {},
                             stream: {},
                         },
-                        automation: {},
-                        network: {
-                            airGap: inputs.ENABLE_NETWORK_AIRGAP,
-                        },
                     },
-                    project: {},
                 },
             };
             const azureRepositoryName = taskLib.getVariable(azure_1.AZURE_ENVIRONMENT_VARIABLES.AZURE_REPOSITORY) || "";
@@ -1515,7 +1503,7 @@ class SynopsysToolsParameter {
                             : "";
                 }
             }
-            if (inputs.COVERITY_LOCAL) {
+            if ((0, utility_1.parseToBoolean)(inputs.COVERITY_LOCAL)) {
                 covData.data.coverity.local = true;
             }
             if (inputs.COVERITY_INSTALL_DIRECTORY) {
@@ -1534,10 +1522,13 @@ class SynopsysToolsParameter {
                 console.info("Coverity Automation comment is enabled");
                 covData.data.azure = yield this.getAzureRepoInfo();
                 covData.data.environment = this.setEnvironmentScanPullData();
-                covData.data.coverity.automation.prcomment = true;
+                covData.data.coverity.automation = { prcomment: true };
             }
             if (inputs.COVERITY_VERSION) {
                 covData.data.coverity.version = inputs.COVERITY_VERSION;
+            }
+            if ((0, utility_1.parseToBoolean)(inputs.ENABLE_NETWORK_AIRGAP)) {
+                covData.data.coverity.network = { airGap: true };
             }
             const inputJson = JSON.stringify(covData);
             let stateFilePath = path_1.default.join(this.tempDir, SynopsysToolsParameter.COVERITY_STATE_FILE_NAME);
@@ -1672,42 +1663,42 @@ class SynopsysToolsParameter {
         return {};
     }
     setSarifReportsInputsForBlackduck() {
-        var _a;
-        const sarifReportFilterSeverities = [];
-        let sarifReportFilePath = "";
-        if (inputs.BLACKDUCK_URL &&
-            ((_a = inputs.BLACKDUCK_REPORTS_SARIF_FILE_PATH) === null || _a === void 0 ? void 0 : _a.trim())) {
-            sarifReportFilePath = inputs.BLACKDUCK_REPORTS_SARIF_FILE_PATH.trim();
+        const reportData = {
+            sarif: {
+                create: true,
+            },
+        };
+        if (inputs.BLACKDUCK_URL && inputs.BLACKDUCK_REPORTS_SARIF_FILE_PATH) {
+            reportData.sarif.file = {
+                path: inputs.BLACKDUCK_REPORTS_SARIF_FILE_PATH,
+            };
         }
+        const sarifReportFilterSeverities = [];
         if (inputs.BLACKDUCK_URL &&
             inputs.BLACKDUCK_REPORTS_SARIF_SEVERITIES &&
             inputs.BLACKDUCK_REPORTS_SARIF_SEVERITIES.length > 0) {
             const sarifSeverities = inputs.BLACKDUCK_REPORTS_SARIF_SEVERITIES.filter((severity) => severity && severity.trim() !== "").map((severity) => severity.trim());
             sarifReportFilterSeverities.push(...sarifSeverities);
         }
-        let groupSCAIssues = true;
+        if (sarifReportFilterSeverities.length > 0) {
+            reportData.sarif.severities = sarifReportFilterSeverities;
+        }
         if (inputs.BLACKDUCK_URL &&
             (0, utility_1.isBoolean)(inputs.BLACKDUCK_REPORTS_SARIF_GROUP_SCA_ISSUES)) {
-            groupSCAIssues = JSON.parse(inputs.BLACKDUCK_REPORTS_SARIF_GROUP_SCA_ISSUES);
+            reportData.sarif.groupSCAIssues = JSON.parse(inputs.BLACKDUCK_REPORTS_SARIF_GROUP_SCA_ISSUES);
         }
-        const reportData = {
-            sarif: {
-                create: true,
-                severities: sarifReportFilterSeverities,
-                file: {
-                    path: sarifReportFilePath,
-                },
-                groupSCAIssues: groupSCAIssues,
-            },
-        };
         return reportData;
     }
     setSarifReportsInputsForPolaris() {
-        var _a;
-        let sarifReportFilePath = "";
-        if (inputs.POLARIS_SERVER_URL &&
-            ((_a = inputs.POLARIS_REPORTS_SARIF_FILE_PATH) === null || _a === void 0 ? void 0 : _a.trim())) {
-            sarifReportFilePath = inputs.POLARIS_REPORTS_SARIF_FILE_PATH.trim();
+        const reportData = {
+            sarif: {
+                create: true,
+            },
+        };
+        if (inputs.POLARIS_SERVER_URL && inputs.POLARIS_REPORTS_SARIF_FILE_PATH) {
+            reportData.sarif.file = {
+                path: inputs.POLARIS_REPORTS_SARIF_FILE_PATH,
+            };
         }
         const sarifReportFilterSeverities = [];
         if (inputs.POLARIS_SERVER_URL &&
@@ -1716,10 +1707,12 @@ class SynopsysToolsParameter {
             const severities = inputs.POLARIS_REPORTS_SARIF_SEVERITIES.filter((severity) => severity && severity.trim() !== "").map((severity) => severity.trim());
             sarifReportFilterSeverities.push(...severities);
         }
-        let groupSCAIssues = true;
+        if (sarifReportFilterSeverities.length > 0) {
+            reportData.sarif.severities = sarifReportFilterSeverities;
+        }
         if (inputs.POLARIS_SERVER_URL &&
             (0, utility_1.isBoolean)(inputs.POLARIS_REPORTS_SARIF_GROUP_SCA_ISSUES)) {
-            groupSCAIssues = JSON.parse(inputs.POLARIS_REPORTS_SARIF_GROUP_SCA_ISSUES);
+            reportData.sarif.groupSCAIssues = JSON.parse(inputs.POLARIS_REPORTS_SARIF_GROUP_SCA_ISSUES);
         }
         const sarifReportIssueTypes = [];
         if (inputs.POLARIS_SERVER_URL &&
@@ -1728,19 +1721,9 @@ class SynopsysToolsParameter {
             const issueTypes = inputs.POLARIS_REPORTS_SARIF_ISSUE_TYPES.filter((issueType) => issueType && issueType.trim() !== "").map((issueType) => issueType.trim());
             sarifReportIssueTypes.push(...issueTypes);
         }
-        const reportData = {
-            sarif: {
-                create: true,
-                severities: sarifReportFilterSeverities,
-                file: {
-                    path: sarifReportFilePath,
-                },
-                issue: {
-                    types: sarifReportIssueTypes,
-                },
-                groupSCAIssues: groupSCAIssues,
-            },
-        };
+        if (sarifReportIssueTypes.length > 0) {
+            reportData.sarif.issue = { types: sarifReportIssueTypes };
+        }
         return reportData;
     }
 }
