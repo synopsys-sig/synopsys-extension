f391ca090d8f7fbdb5466cd77bcfc930481cd377
Akib Uz Zaman <akibuz@synopsys.com>
2024-06-10 21:35:15 +0600

    update unit test in tool-parameters.spec.ts

--- synopsys-task/test/unit/synopsys-task/tools-parameter.spec.ts
+++ synopsys-task/test/unit/synopsys-task/tools-parameter.spec.ts
@@ -720,7 +720,7 @@ describe("Synopsys Tools Parameter test", () => {
             sandbox.restore();
         });
 
-         it('should success for blackduck command formation with mandatory and some optional parameters', function () {
+         it('should success for blackduck command formation with mandatory and some optional parameters', async function () {
             Object.defineProperty(inputs, 'BLACKDUCK_URL', {value: 'https://test.com'})
             Object.defineProperty(inputs, 'BLACKDUCK_API_TOKEN', {value: 'token'})
             Object.defineProperty(inputs, 'BLACKDUCK_INSTALL_DIRECTORY', {value: 'test'})
@@ -728,15 +728,15 @@ describe("Synopsys Tools Parameter test", () => {
             Object.defineProperty(inputs, 'BLACKDUCK_SCAN_FAILURE_SEVERITIES', {value : ["BLOCKER","CRITICAL","TRIVIAL"]})
             
              sandbox.stub(validator, "validateBlackduckFailureSeverities").returns(true);
-             const formattedCommand = synopsysToolsParameter.getFormattedCommandForBlackduck();
+             const formattedCommand = await synopsysToolsParameter.getFormattedCommandForBlackduck();
              const jsonString = fs.readFileSync(blackduckStateFile, 'utf-8');
              const jsonData = JSON.parse(jsonString);
              expect(jsonData.data.blackduck.url).to.be.equals('https://test.com');
              expect(jsonData.data.blackduck.token).to.be.equals('token');    
-             expect(formattedCommand).contains(Promise.resolve('--stage blackduck'));
+             expect(formattedCommand).contains('--stage blackduck');
 
              blackduckStateFile = '"'.concat(blackduckStateFile).concat('"');
-             expect(formattedCommand).contains(Promise.resolve('--input '.concat(blackduckStateFile)));
+             expect(formattedCommand).contains('--input '.concat(blackduckStateFile));
          });
 
          it('should success for blackduck command formation with PR Comment in PR context', async function () {
